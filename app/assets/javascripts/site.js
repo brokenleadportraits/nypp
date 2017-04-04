82e7e5b5601c81dee9fcf28c02ea7b495ccee1ee
Alex <alexthetford@gmail.com>
2017-04-03 21:27:40 -0400

    trying to fix raty images on heroku

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
+    $('.rating').raty( { path: '/public/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/public/assets',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
